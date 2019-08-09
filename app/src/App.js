5417c0ccba31b87a22474200db9f469f2f9ade7e
Tim <timskap@gmail.com>
2019-08-09 19:31:06 +0300

    [Add] Нормальный поиск, добавил проверки

--- app/src/App.js
+++ app/src/App.js
@@ -15,16 +15,39 @@ class App extends React.Component {
 		this.onChange = this.onChange.bind(this)
 	}
 
-	onChange(search) {
+	Translitirate(word) {
+		if (word != undefined) {
+			var A = {};
+			var result = '';
+	
+			A["Ё"] = "YO"; A["Й"] = "I"; A["Ц"] = "TS"; A["У"] = "U"; A["К"] = "K"; A["Е"] = "E"; A["Н"] = "N"; A["Г"] = "G"; A["Ш"] = "SH"; A["Щ"] = "SCH"; A["З"] = "Z"; A["Х"] = "H"; A["Ъ"] = "'";
+			A["ё"] = "yo"; A["й"] = "i"; A["ц"] = "ts"; A["у"] = "u"; A["к"] = "k"; A["е"] = "e"; A["н"] = "n"; A["г"] = "g"; A["ш"] = "sh"; A["щ"] = "sch"; A["з"] = "z"; A["х"] = "h"; A["ъ"] = "'";
+			A["Ф"] = "F"; A["Ы"] = "I"; A["В"] = "V"; A["А"] = "A"; A["П"] = "P"; A["Р"] = "R"; A["О"] = "O"; A["Л"] = "L"; A["Д"] = "D"; A["Ж"] = "ZH"; A["Э"] = "E";
+			A["ф"] = "f"; A["ы"] = "i"; A["в"] = "v"; A["а"] = "a"; A["п"] = "p"; A["р"] = "r"; A["о"] = "o"; A["л"] = "l"; A["д"] = "d"; A["ж"] = "zh"; A["э"] = "e";
+			A["Я"] = "YA"; A["Ч"] = "CH"; A["С"] = "S"; A["М"] = "M"; A["И"] = "I"; A["Т"] = "T"; A["Ь"] = "'"; A["Б"] = "B"; A["Ю"] = "YU";
+			A["я"] = "ya"; A["ч"] = "ch"; A["с"] = "s"; A["м"] = "m"; A["и"] = "i"; A["т"] = "t"; A["ь"] = "'"; A["б"] = "b"; A["ю"] = "yu";
+	
+			for (var i = 0; i < word.length; i++) {
+				var c = word.charAt(i);
+	
+				result += A[c] || c;
+			}
+		
+			return result.replace(/[\W_]+/g, " ");
+		} else {
+			return ""
+		}
+	}
 
+	onGetResult(search){ 
 		this.setState({ 
-			search : search,
 			games : [] 
 		}, function() {
-			fetch(`https://api.rawg.io/api/games/${this.state.search.replace(/ /g, "-")}/suggested?page_size=5`)
+			fetch(`https://api.rawg.io/api/games/${search}/suggested?page_size=5`)
 			.then(res => res.json())
 			.then(data => {
 				if (typeof data.results !== 'undefined') {
+					console.log(data.results)
 					this.setState({
 						games : data.results
 					})
@@ -33,9 +56,36 @@ class App extends React.Component {
 		});
 	}
 
+	onChange(search) {
+
+		this.setState({ 
+			search : search,
+		}, function() {
+			fetch(`https://api.rawg.io/api/games?page_size=5&search=${this.Translitirate(search)}`)
+			.then(res => res.json())
+			.then(data => {
+				if (typeof data.results !== 'undefined') {
+					console.log(data.results)
+					if (data.results[0] != null) {
+						this.onGetResult(data.results[0].slug)
+					}
+				}
+			})
+		});
+	}
+
 	getYear(gameDate) {
 		return new Date(gameDate).getFullYear();
 	}
+
+	isMetacriticAvailable(score) {
+		if (score != undefined) {
+			return score;
+		} else {
+			return "Неизвестно"
+		}
+	}
+
 
 	render() {
 		return (
@@ -61,11 +111,10 @@ class App extends React.Component {
 								}}>
 									<h2>{game.name}</h2>
 									<h3>Дата выхода игры: {this.getYear(game.released)}</h3>
-									<h4>Metacritic: {game.metacritic}</h4>
-									<h3>
-										
-										{ game.genres.map((gen, index) => (
-											<span key={index}>{gen.name}, </span>
+									<h4>Metacritic: {this.isMetacriticAvailable(game.metacritic)}</h4>
+									<h3>В жанре: {this.state.games.length > 0 &&
+										 game.genres.map((gen, index) => (
+											<span key={index}>{gen.name} </span>
 										))}
 									</h3>
 									<p>{game.short_description}</p>
@@ -76,17 +125,21 @@ class App extends React.Component {
 										))
 										}
 									</h3>
-									<div>
-                                            {
-                                                game.stores.map((store, index) => (
-                                                    <a href={store.url_en}>{store.url_en}</a>
+									<div style={{
+										paddingBottom: 16
+									}}>
+										<h3>💵Можно купить в магазинах</h3>
+										
+										{
+											game.stores.map((store, index) => (
+													<span><a href={store.url_en}>{store.store.name}</a> </span>
 												))
 												
                                             }
                                     </div>
 									<Gallery
-										slideWidth="100%"
-										style={{ height: 270 }}
+										slideWidth="90%"
+										style={{ height: 400 }}
 									>
 										{
 											game.short_screenshots.map((screen, index) => (
